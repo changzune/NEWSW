963169e7409ecdea18578dbdfcb3ef43a6b6ca4e
changzunelee <changzunelee@gmail.com>
2022-01-29 22:58:05 +0900

    #

---
+++ NEWSW/src/ex12/APhone.java
@@ -0,0 +1,23 @@
+package ex12;
+
+public class APhone implements IFunction{
+
+	@Override
+	public void callSenderReceive() {
+		// TODO Auto-generated method stub
+		System.out.println("媛��뒫 �빀�땲�떎.");
+	}
+
+	@Override
+	public void canLte() {
+		// TODO Auto-generated method stub
+		System.out.println("遺덇��뒫 �빀�땲�떎. 3G �엯�땲�떎.");
+	}
+
+	@Override
+	public void tvRemoteController() {
+		// TODO Auto-generated method stub
+		System.out.println("誘명깙�옱 �릺�뼱 �엳�뒿�땲�떎.");
+	}
+
+}
---
+++ NEWSW/src/ex12/CPhone.java
@@ -0,0 +1,23 @@
+package ex12;
+
+public class CPhone implements IFunction {
+
+	@Override
+	public void callSenderReceive() {
+		// TODO Auto-generated method stub
+		System.out.println("媛��뒫 �빀�땲�떎.");
+	}
+
+	@Override
+	public void canLte() {
+		// TODO Auto-generated method stub
+		System.out.println("媛��뒫 �빀�땲�떎. 4G �엯�땲�떎.");
+	}
+
+	@Override
+	public void tvRemoteController() {
+		// TODO Auto-generated method stub
+		System.out.println("誘명깙�옱 �릺�뼱 �엳�뒿�땲�떎.");
+	}
+
+}
