6ef72349bb7bf4096796bdbb1b115bf9fa2b665d
changzunelee <changzunelee@gmail.com>
2022-01-31 22:57:00 +0900

    #

---
+++ NEWSW/src/ex14/StandardRobot.java
@@ -0,0 +1,41 @@
+package ex14;
+
+public class StandardRobot {
+	public StandardRobot() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void actionWalk() {
+		// TODO Auto-generated method stub
+		System.out.println("걷을 수 있습니다.");
+
+	}
+	
+	public void actionrun() {
+		System.out.println("달릴 수 있습니다.");
+
+	}
+	
+	public void actionFly() {
+		System.out.println("날 수 없습니다.");
+
+	}
+	
+	public void actionMisail() {
+		System.out.println("미사일을 쏠 수 있습니다.");
+
+	}
+	
+	public void actionKnife() {
+		System.out.println("목검이 있습니다.");
+
+	}
+	
+	public void shape() {
+		System.out.println("SuperRobot 입니다. 외형은 팔,다리,몸통,머리가 있습니다.");
+
+	}
+	
+	
+	
+}
