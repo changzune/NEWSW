fb54152039ed80fbe20546d9dcee6b74aa246ac6
changzunelee <changzunelee@gmail.com>
2022-02-03 23:48:46 +0900

    #

---
+++ NEWSW/src/ex20/MainClass.java
@@ -0,0 +1,18 @@
+package ex20;
+
+import java.util.HashSet;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		
+		HashSet<Student> hashset = new HashSet<Student>();
+		hashset.add(new Student("홍길동", 3));
+		hashset.add(new Student("이순신", 6));
+		hashset.add(new Student("장보고", 1));
+		
+		
+	}
+
+}
