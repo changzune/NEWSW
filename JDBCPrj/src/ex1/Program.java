178713ddf1a978161ae93d760c05be0ba10552e2
zune <changzunelee@gmail.com>
2022-03-10 23:35:30 +0900

    #

--- JDBCPrj/src/ex1/Program.java
+++ JDBCPrj/src/ex1/Program.java
@@ -1,9 +1,23 @@
 package ex1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.15:1521/xepdb1";,"scott","tiger");
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		rs.close();
+		st.close();
+		con.close();
+		
 
 	}
 
