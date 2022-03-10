c5d4ea9e9aa33fd76458a7d9880b1d49ce2abbb6
zune <changzunelee@gmail.com>
2022-03-10 23:41:06 +0900

    #

--- JDBCPrj/src/ex1/Program.java
+++ JDBCPrj/src/ex1/Program.java
@@ -3,14 +3,18 @@ package ex1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		String url = "jdbc:oracle:thin:@192.168.0.15:1521/xepdb1";
+		String sql ="SELECT * FROM NOTICE";
 		
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.15:1521/xepdb1";,"scott","tiger");
+		Connection con = DriverManager.getConnection(url,"scott","tiger");
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(sql);
 		
