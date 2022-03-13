a496cd5d0ae8f578fb420c4d3971804f405473d3
zune <changzunelee@gmail.com>
2022-03-13 22:47:43 +0900

    #

--- JDBCPrj/src/ex1/Program.java
+++ JDBCPrj/src/ex1/Program.java
@@ -31,8 +31,7 @@ public class Program {
 		Date regDate = rs.getDate("REGDATE");
 		String content = rs.getString("CONTENT");
 		int hit = rs.getInt("hit");
-		
-		
+
 		System.out.printf(" id:%d, title:%s, Writerld:%s, regDate:%s, content:%s, hit:%d\n",
 							id, title, Writerld, regDate,content, hit);
 
---
+++ JDBCPrj/src/ex1/Program2.java
@@ -0,0 +1,48 @@
+package ex1;
+
+import java.util.Date;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program2 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		String title="TEST2";
+		String writerId ="newlec";
+		String content = "hahaha";
+		String files = "";
+		
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String sql = "INSERT INTO NOTICE ( " +
+				" title," +
+				" writer_id," +
+				" content," +
+				" files"	+
+				") VALUES (?,?,?,?)";
+				
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url,"scott","tiger");
+		//Statement st = con.createStatement();
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, title);
+		st.setString(2, writerId);
+		st.setString(3, content);
+		st.setString(4, files);
+
+		
+		int result = st.executeUpdate();
+		System.out.println(result);
+	
+		st.close();
+		con.close();
+		
+	}
+
+}
