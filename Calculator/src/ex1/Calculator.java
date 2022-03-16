67898db9453ab692d59d875939b59d42ad996a85
zune <changzunelee@gmail.com>
2022-03-16 22:27:24 +0900

    #

---
+++ Calculator/src/ex1/Calculator.java
@@ -0,0 +1,13 @@
+package ex1;
+
+public class Calculator {
+	public Calculator() {
+	
+	}
+
+	public static int add(int x, int y) {
+		return x+y;
+		
+	}
+
+}
---
+++ Calculator/src/ex1/Program.java
@@ -0,0 +1,12 @@
+package ex1;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		Calculator.add(3,4);
+		
+		
+	}
+
+}
